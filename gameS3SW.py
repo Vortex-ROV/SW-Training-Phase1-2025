8763b8e84abeae15bb041476fafdd1f214f2b5f3
youssefShokier <youssefmahmoud0494@gmail.com>
2024-09-13 00:30:43 +0300

    main menu

---
+++ gameS3SW.py
@@ -0,0 +1,10 @@
+while True:
+ print("welcome to our game the playgroud\n1) play\n2) leaderBord\n3) Exit")
+ x = int(input("please enter 1,2,3"))
+ match x:
+  case 1 :
+      print("rrr") #mkan elprint htb2a elmethod
+  case 2 :
+      print("eee")    #mkan elprint htb2a elmethod
+  case 3 :
+      break    
