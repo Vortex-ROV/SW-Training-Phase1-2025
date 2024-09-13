00c154a60ac804a928808def3c5cf32aed0fc2a5
youssefShokier <youssefmahmoud0494@gmail.com>
2024-09-13 21:26:13 +0300

    main menu

--- S3SW/gameS3SW.py
+++ S3SW/gameS3SW.py
@@ -3,8 +3,8 @@ while True:
  x = int(input("please enter 1,2,3"))
  match x:
   case 1 :
-      print("rrr") #mkan elprint htb2a elmethod
+      print("rrr")    #mkan el print htb2a elmethod
   case 2 :
-      print("eee")    #mkan elprint htb2a elmethod
+      print("eee")    #mkan el print htb2a elmethod
   case 3 :
       break    
