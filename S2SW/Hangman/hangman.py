308d16936bfaf9d07409df0e246b05116c448e64
Vitroin <mohamed.elsayedt10@gmail.com>
2024-09-19 01:33:27 +0300

    Added HangMan Leaderboard - Repeat Functionality for Hangman and Score Functionality
    Improved merge files functionality

--- S2SW/Hangman/hangman.py
+++ S2SW/Hangman/hangman.py
@@ -1,4 +1,5 @@
 def hangman():
+    allScores = []
     import time
     import random
 
@@ -42,37 +43,83 @@ def hangman():
     def answer(answer):
         pass
 
+    goOn = True
+    while goOn:
+        maxScore = 0
+        answer = random.choice(Words)
+        guessed_letters = set()
 
-    answer = random.choice(Words)
-    guessed_letters = set()
-    hint = ["_"] * len(answer)
-    incorrect_guesses = 0
-    while True:
-        hangman_display(incorrect_guesses)
-        spaces(hint)
-        guessed_L = input("Enter a letter").lower()
-        while len(guessed_L) != 1 or not guessed_L.isalpha():
-            guessed_L = input("Please select a singular alphabetical letter")
-            if guessed_L in guessed_letters():
-                print("YOU ALREADY GUESSED THIS LETTER")
-        if guessed_L in answer:
-            for i in range(len(answer)):
-                if answer[i] == guessed_L:
-                    hint[i] = guessed_L
-                    guessed_letters.add(guessed_L)
-        else:
-            incorrect_guesses += 1
+        hint = ["_"] * len(answer)
+        incorrect_guesses = 0
+        while True:
             hangman_display(incorrect_guesses)
-        if incorrect_guesses >= 6:
-            hangman_display(incorrect_guesses)
-            print(f"YOU LOST {name} ")
-            print()
-            print(f"THE ANSWER WAS {answer}")
-            print()
-            break
-        if "_" not in hint:
-            print()
             spaces(hint)
-            print()
-            print(f"YOU WON {name} ")
-            break
+            guessed_L = input("Enter a letter").lower()
+            while len(guessed_L) != 1 or not guessed_L.isalpha():
+                guessed_L = input("Please select a singular alphabetical letter")
+                if guessed_L in guessed_letters():
+                    print("YOU ALREADY GUESSED THIS LETTER")
+            if guessed_L in answer:
+                maxScore +=1
+                
+                for i in range(len(answer)):
+                    if answer[i] == guessed_L:
+                        hint[i] = guessed_L
+                        guessed_letters.add(guessed_L)
+            else:
+                incorrect_guesses += 1
+                hangman_display(incorrect_guesses)
+            if incorrect_guesses >= 6:
+                hangman_display(incorrect_guesses)
+                allScores.append(maxScore)
+                print(f"YOU LOST {name} ")
+                print()
+                print(f"THE ANSWER WAS {answer}")
+                print()
+                break
+            if "_" not in hint:
+                print()
+                spaces(hint)
+                print()
+                print(f"YOU WON {name} ")
+                allScores.append(maxScore)
+                break
+        while True:
+            try:
+                gameContinue = input("Do You Want To Play Again? Y/N  ").lower().strip()
+                if gameContinue not in ["y", "n"]:
+                    raise ValueError("Invalid choice. Please enter Y or N.")
+                break
+            except ValueError as e:
+                print(e)
+        if gameContinue == "y":
+            continue
+        else:
+            goOn = False
+    print(allScores)
+
+    try:
+        with open(r"C:\Users\ACER\Desktop\Vortex\Training Phase 1\Python Fundementals\SW-Training-Phase1-2025\S2SW\Hangman\hangmanLeaderBoard.txt", 'r') as file:
+            allLeaderBoard = file.readlines()
+    except FileNotFoundError:
+        allLeaderBoard = []
+
+    try:
+        allLeaderBoard = [int(line.strip()) for line in allLeaderBoard if line.strip().isdigit()]
+    except ValueError:
+        print("Error reading leaderboard data. Some entries might be corrupted.")
+        return
+
+    allLeaderBoard.extend(allScores)
+    allLeaderBoard = list(set(allLeaderBoard)) 
+    allLeaderBoard.sort(reverse=True)
+
+    with open(r"C:\Users\ACER\Desktop\Vortex\Training Phase 1\Python Fundementals\SW-Training-Phase1-2025\S2SW\Hangman\hangmanLeaderBoard.txt", 'w') as file:
+        for score in allLeaderBoard:
+            file.write(f"{str(score)}\n")
+
+    print("Leaderboard Updated:", allLeaderBoard)
+
+
+
+hangman()
--- S2SW/Main-Game/main.py
+++ S2SW/Main-Game/main.py
@@ -1,6 +1,6 @@
 def guessNumber():
     import random
-    
+
     goOn = True
     scores = []
 
@@ -27,7 +27,8 @@ def guessNumber():
 
         while True:
             try:
-                gameContinue = input("Do You Want To Play Again? Y/N  ").lower().strip()
+                gameContinue = input(
+                    "Do You Want To Play Again? Y/N  ").lower().strip()
                 if gameContinue not in ["y", "n"]:
                     raise ValueError("Invalid choice. Please enter Y or N.")
                 break
@@ -39,7 +40,6 @@ def guessNumber():
         else:
             print(scores)
             goOn = False
-
 
     try:
         with open("guessNumberLeaderboard.txt", 'r') as file:
@@ -78,7 +78,7 @@ def quizGame():
     gameOn = True
     while gameOn:
 
-        questionAmount = len(linesList) // 6 
+        questionAmount = len(linesList) // 6
         lives = 3
         i = 0
         points = 0
@@ -92,9 +92,11 @@ def quizGame():
 
             while True:
                 try:
-                    userAnswer = input("Please Enter A/B/C/D: ").strip().lower()
+                    userAnswer = input(
+                        "Please Enter A/B/C/D: ").strip().lower()
                     if userAnswer not in ["a", "b", "c", "d"]:
-                        raise ValueError("Invalid choice. Please enter A/B/C/D")
+                        raise ValueError(
+                            "Invalid choice. Please enter A/B/C/D")
                     break
                 except ValueError as e:
                     print(e)
@@ -115,7 +117,8 @@ def quizGame():
 
         while True:
             try:
-                gameContinue = input("Do You Want To Play Again? Y/N  ").lower().strip()
+                gameContinue = input(
+                    "Do You Want To Play Again? Y/N  ").lower().strip()
                 if gameContinue not in ["y", "n"]:
                     raise ValueError("Invalid choice. Please enter Y or N.")
                 break
@@ -132,13 +135,14 @@ def quizGame():
         allLeaderBoard = []
 
     try:
-        allLeaderBoard = [int(line.strip()) for line in allLeaderBoard if line.strip().isdigit()]
+        allLeaderBoard = [int(line.strip())
+                              for line in allLeaderBoard if line.strip().isdigit()]
     except ValueError:
         print("Error reading leaderboard data. Some entries might be corrupted.")
         return
 
     allLeaderBoard.extend(scores)
-    allLeaderBoard = list(set(allLeaderBoard)) 
+    allLeaderBoard = list(set(allLeaderBoard))
     allLeaderBoard.sort(reverse=True)
 
     with open("QuizGame Leaderboard.txt", 'w') as file:
@@ -149,7 +153,7 @@ def quizGame():
 
 
 def hangman():
-
+    allScores = []
     import time
     import random
 
@@ -193,63 +197,227 @@ def hangman():
     def answer(answer):
         pass
 
+    goOn = True
+    while goOn:
+        maxScore = 0
+        answer = random.choice(Words)
+        guessed_letters = set()
 
-    answer = random.choice(Words)
-    guessed_letters = set()
-    hint = ["_"] * len(answer)
-    incorrect_guesses = 0
-    while True:
-        hangman_display(incorrect_guesses)
-        spaces(hint)
-        guessed_L = input("Enter a letter").lower()
-        while len(guessed_L) != 1 or not guessed_L.isalpha():
-            guessed_L = input("Please select a singular alphabetical letter")
-            if guessed_L in guessed_letters():
-                print("YOU ALREADY GUESSED THIS LETTER")
-        if guessed_L in answer:
-            for i in range(len(answer)):
-                if answer[i] == guessed_L:
-                    hint[i] = guessed_L
-                    guessed_letters.add(guessed_L)
-        else:
-            incorrect_guesses += 1
+        hint = ["_"] * len(answer)
+        incorrect_guesses = 0
+        while True:
             hangman_display(incorrect_guesses)
-        if incorrect_guesses >= 6:
-            hangman_display(incorrect_guesses)
-            print(f"YOU LOST {name} ")
-            print()
-            print(f"THE ANSWER WAS {answer}")
-            print()
-            break
-        if "_" not in hint:
-            print()
             spaces(hint)
-            print()
-            print(f"YOU WON {name} ")
-            break
+            guessed_L = input("Enter a letter").lower()
+            while len(guessed_L) != 1 or not guessed_L.isalpha():
+                guessed_L = input("Please select a singular alphabetical letter")
+                if guessed_L in guessed_letters():
+                    print("YOU ALREADY GUESSED THIS LETTER")
+            if guessed_L in answer:
+                maxScore +=1
+                
+                for i in range(len(answer)):
+                    if answer[i] == guessed_L:
+                        hint[i] = guessed_L
+                        guessed_letters.add(guessed_L)
+            else:
+                incorrect_guesses += 1
+                hangman_display(incorrect_guesses)
+            if incorrect_guesses >= 6:
+                hangman_display(incorrect_guesses)
+                allScores.append(maxScore)
+                print(f"YOU LOST {name} ")
+                print()
+                print(f"THE ANSWER WAS {answer}")
+                print()
+                break
+            if "_" not in hint:
+                print()
+                spaces(hint)
+                print()
+                print(f"YOU WON {name} ")
+                allScores.append(maxScore)
+                break
+        while True:
+            try:
+                gameContinue = input("Do You Want To Play Again? Y/N  ").lower().strip()
+                if gameContinue not in ["y", "n"]:
+                    raise ValueError("Invalid choice. Please enter Y or N.")
+                break
+            except ValueError as e:
+                print(e)
+        if gameContinue == "y":
+            continue
+        else:
+            goOn = False
+    print(allScores)
+
+    try:
+        with open(r"C:\Users\ACER\Desktop\Vortex\Training Phase 1\Python Fundementals\SW-Training-Phase1-2025\S2SW\Hangman\hangmanLeaderBoard.txt", 'r') as file:
+            allLeaderBoard = file.readlines()
+    except FileNotFoundError:
+        allLeaderBoard = []
+
+    try:
+        allLeaderBoard = [int(line.strip()) for line in allLeaderBoard if line.strip().isdigit()]
+    except ValueError:
+        print("Error reading leaderboard data. Some entries might be corrupted.")
+        return
+
+    allLeaderBoard.extend(allScores)
+    allLeaderBoard = list(set(allLeaderBoard)) 
+    allLeaderBoard.sort(reverse=True)
+
+    with open(r"C:\Users\ACER\Desktop\Vortex\Training Phase 1\Python Fundementals\SW-Training-Phase1-2025\S2SW\Hangman\hangmanLeaderBoard.txt", 'w') as file:
+        for score in allLeaderBoard:
+            file.write(f"{str(score)}\n")
+
+    print("Leaderboard Updated:", allLeaderBoard)
+
+
+def startleaderboard():
+
+    def leaderboardHangman():
+        
+        try:
+            with open(r"C:\Users\ACER\Desktop\Vortex\Training Phase 1\Python Fundementals\SW-Training-Phase1-2025\S2SW\Hangman\hangmanLeaderBoard.txt", 'r') as file:
+                linesList = file.readlines()
+        except FileNotFoundError:
+            print("Questions file not found.")
+            return
+        print("\nTOP SCORES WITH MOST ANSWERED QUESTIONS!")
+        print("="*42)
+        for lines in linesList:
+            print(lines)
+            print("="*42)
+
+    def leaderboardQuizGame():
+        try:
+            with open(r"C:\Users\ACER\Desktop\Vortex\Training Phase 1\Python Fundementals\SW-Training-Phase1-2025\S2SW\Main-Game\QuizGame Leaderboard.txt", 'r') as file:
+                linesList = file.readlines()
+        except FileNotFoundError:
+            print("Questions file not found.")
+            return
+        print("\nTOP SCORES WITH MOST ANSWERED QUESTIONS!")
+        print("="*42)
+        for lines in linesList:
+            print(lines)
+            print("="*42)
+
+    def leadebroardGuessingNumber():
+        try:
+            with open(r"C:\Users\ACER\Desktop\Vortex\Training Phase 1\Python Fundementals\SW-Training-Phase1-2025\S2SW\Main-Game\guessNumberLeaderboard.txt", 'r') as file:
+                linesList = file.readlines()
+        except FileNotFoundError:
+            print("Questions file not found.")
+            return
+        print("\n TOP SCORES WITH LEAST AMOUNT OF TRIES!")
+        print("="*42)
+        for lines in linesList:
+            print(lines)
+            print("="*42)
+
+    def leaderboardTicTacToe():
+        pass
+
+    def leaderboardWordScramble():
+        pass
+
+    def leaderboard():
+        print(f"\nGame Options:")
+        print(f"LeaderBoard Hangman: 1")
+        print(f"LeaderBoard Quiz-Game: 2")
+        print(f"LeaderBoard Guessing-Number: 3")
+        print(f"LeaderBoard Tic-Tac-Toe: 4")
+        print(f"LeaderBoard Word-Scramble: 5")
+
+        while True:
+            try:
+                num = int(input("\nEnter a number between 1 and 5: "))
+                if num not in [1, 2, 3, 4, 5]:
+                    raise ValueError("Invalid choice. Please enter number 1-5")
+                break
+            except ValueError as e:
+                print("Invalid choice. Please enter number 1-5")
+
+        match num:
+            case 1:
+                leaderboardHangman()
+                breakpoint
+            case 2:
+                leaderboardQuizGame()
+                breakpoint
+            case 3:
+                leadebroardGuessingNumber()
+                breakpoint
+            case 4:
+                pass
+            case 5:
+                pass
+            case _:
+                print("\nNumber not between 1 and 5")
+
+    leaderboardRepeats = True
+    while leaderboardRepeats:
+
+        leaderboard()
+
+        while True:
+            try:
+                repeatsInput = input("Do you want to see another leaderboard? Y/N  ").lower().strip()
+                if repeatsInput not in ["y", "n"]:
+                    raise ValueError("Invalid choice. Please enter Y or N.")
+                break
+            except ValueError as e:
+                print(e)
+
+        if repeatsInput == "n":
+            leaderboardRepeats = False
+
 
 
 def runMatch():
-    num = int(input("Enter a number between 1 and 3: "))
+    print(f"\nGame Options:")
+    print(f"Hangman: 1")
+    print(f"Quiz-Game: 2")
+    print(f"Guessing-Number: 3")
+    print(f"Tic-Tac-Toe: 4")
+    print(f"Word-Scramble: 5")
+    print(f"Leaderboards: 6")
+
+    while True:
+        try:
+            num = int(input("\nEnter a number between 1 and 6: "))
+            if num not in [1, 2, 3, 4, 5,6]:
+                raise ValueError("Invalid choice. Please enter number 1-6")
+            break
+        except ValueError as e:
+            print("Invalid choice. Please enter number 1-6")
+
     match num:
         case 1:
-            guessNumber()
+            hangman()
             breakpoint
         case 2:
             quizGame()
             breakpoint
         case 3:
-            hangman()
+            guessNumber()
+            breakpoint
+        case 4:
+            pass
+        case 5:
+            pass
+        case 6:
+            startleaderboard()
             breakpoint
         case _:
-            print("Number not between 1 and 3")
-        
+            print("\nNumber not between 1 and 6")
         
 gameRepeats = True
 while gameRepeats:
     
     runMatch()
-
     while True:
         try:
             continueInput = input("Do you want to try another game? Y/N  ").lower().strip()
