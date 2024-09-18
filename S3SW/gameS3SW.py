dfdabd9dc9eb3e020279be0d0f507fe95b006363
youssefShokier <youssefmahmoud0494@gmail.com>
2024-09-18 17:42:07 +0300

    hang man game done with leader bord

--- S3SW/gameS3SW.py
+++ S3SW/gameS3SW.py
@@ -1,5 +1,6 @@
 from gussingGame.game1 import gussing_game
 from scrambleGame.game2 import scrambleGame
+from hangManGame.game3 import hang_man_game
 
 def play():  #play method that show the games menu
   while True: 
@@ -15,7 +16,7 @@ def play():  #play method that show the games menu
         case 4:
             gussing_game()
         case 5: 
-            print("hangManGame") #mkan el print htb2a elmethod
+            hang_man_game()
         case 6 :
             break    
 def leaderBord():
@@ -36,7 +37,9 @@ def leaderBord():
              print(file.read())
              file.close
         case 5: 
-            print("hangManGame") #mkan el print htb2a elmethod
+             file=open("S3SW\hangManGame\game3.txt")
+             print(file.read())
+             file.close
                  
 while True:
  print("welcome to our game the playgroud\n1) play\n2) leaderBord\n3) Exit")
