cbba0dc89af97fab3338bd1b889a541b3c7933dd
youssefShokier <youssefmahmoud0494@gmail.com>
2024-09-30 23:08:27 +0300

    fixed the code

--- tasks/openCvFourthTask.py
+++ tasks/openCvFourthTask.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np 
 circles = []
 def draw_circle(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
@@ -6,7 +7,7 @@ def draw_circle(event, x, y, flags, param):
 cv2.namedWindow("Draw Circles")
 cv2.setMouseCallback("Draw Circles", draw_circle)
 while True:
-    img = cv2.zeros((512, 512, 3), dtype="uint8")
+    img = np.zeros((512, 512, 3), dtype="uint8")
     for (x, y, radius) in circles:
         cv2.circle(img, (x, y), radius, (0, 255, 0), -1)  
     
