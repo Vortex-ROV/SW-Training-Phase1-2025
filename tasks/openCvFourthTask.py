8bd93db6aa8d4a2ad0adf2c5777463a8c526d684
youssefShokier <youssefmahmoud0494@gmail.com>
2024-09-30 22:38:12 +0300

    fourth task done

---
+++ tasks/openCvFourthTask.py
@@ -0,0 +1,25 @@
+import cv2
+circles = []
+def draw_circle(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        circles.append((x, y, 20))
+cv2.namedWindow("Draw Circles")
+cv2.setMouseCallback("Draw Circles", draw_circle)
+while True:
+    img = cv2.zeros((512, 512, 3), dtype="uint8")
+    for (x, y, radius) in circles:
+        cv2.circle(img, (x, y), radius, (0, 255, 0), -1)  
+    
+    cv2.imshow("Draw Circles", img)
+
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord('d') and circles:
+        circles.pop()
+
+    if key == ord('a'):
+        circles.clear()
+
+    if key == ord('q'):
+        break
+cv2.destroyAllWindows()
