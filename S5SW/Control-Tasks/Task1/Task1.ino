fe0a3a004428144d9bac73d1b0cb7e353b05134a
AmmarElsamman <elsamman321@gmail.com>
2024-12-16 21:40:56 +0200

    Task1-Add-potentiometer

--- S5SW/Control-Tasks/Task1/Task1.ino
+++ S5SW/Control-Tasks/Task1/Task1.ino
@@ -1,15 +1,21 @@
 #define trigPin 12 
 #define echoPin 11
 #define buzzerPin = 8;
+#define potPin = A0;
 long duration, distance;
-const float distanceThreshold = 10.0; 
+float distanceThreshold = 10.0; 
 void setup() {
   Serial.begin (9600); 
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT); 
   pinMode(buzzerPin, OUTPUT);
+  pinMode(potPin, INPUT);
 }
 void loop() {
+  int potValue = analogRead(potPin);
+
+  distanceThreshold = map(potValue, 0, 1023, 1, 50);
+
   digitalWrite(trigPin, LOW); 
   delayMicroseconds(2); 
   digitalWrite(trigPin, HIGH); 
@@ -20,6 +26,7 @@ void loop() {
   distance = (duration/2) * 0.0343; 
   
   Serial.println(distance);
+  Serial.println(distanceThreshold);
   
   if(distance > 0 && distance <= distanceThreshold) {
     digitalWrite(buzzerPin, HIGH);
